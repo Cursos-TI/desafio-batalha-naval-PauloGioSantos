eed1d0d4fc6fa73e3bff6e72ede86631a397ccb6
PauloGioSantos <paulogiosantos@gmail.com>
2025-11-14 13:21:50 +0000

    batalhanaval_mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+/*
+Desafio Batalha Naval - Nível Novato
+Autor: Paulo Giovani dos Santos
+Descrição:
+Este programa cria um tabuleiro 10x10 de Batalha Naval,
+posiciona dois navios (dois horizontais/verticais)
+e exibe o tabuleiro no console.
+*/
 
 
 int main() {
@@ -44,35 +49,7 @@ int main() {
             printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
-    }
-    
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    } 
 
     return 0;
 }
